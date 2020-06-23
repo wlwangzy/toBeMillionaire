5d0ff21d25be44da15e50d90e23c527f87b8f73e
hl c <xhxhongss@gmail.com>
2020-06-24 07:12:21 +0800

    feature: 简化测试代码的输入

--- py_request/py_request.py
+++ py_request/py_request.py
@@ -6,7 +6,8 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 if __name__ == "__main__":
-    requestUrl = 'http://zq.win007.com/analysis/1743046sb.htm#porlet_0'
+    requestId = '1761764'
+    requestUrl = 'http://zq.win007.com/analysis/' + requestId + 'sb.htm#porlet_0'
     header = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36'}
     html = requests.get(requestUrl, headers = header)
     html.encoding = 'utf-8'
@@ -14,10 +15,10 @@ if __name__ == "__main__":
     classHistroyGameData, classInGameData1, classInGameData2 = praseHtmlGameTableData(html.text)
     classIntegralData = praseHtmlIntegralData(html.text)
 
-    requestUrl2 = 'http://zq.win007.com/analysis/odds/1743046.htm?' + str(int(time.time()) * 1000)
+    requestUrl2 = 'http://zq.win007.com/analysis/odds/' + requestId + '.htm?' + str(int(time.time()) * 1000)
 
     header2 = {'User-Agent':'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36',
-                'Referer':'http://zq.win007.com/analysis/1743046sb.htm'}
+                'Referer':'http://zq.win007.com/analysis/' + requestId + 'sb.htm'}
     html = requests.get(requestUrl2, headers = header2)
     html.encoding = 'utf-8'
 
@@ -25,3 +26,5 @@ if __name__ == "__main__":
     classIndexData = praseHtmlIndexData(html.text)
     
     writeExcelData(classHistroyGameData, classInGameData1, classInGameData2, classIndexData, classIntegralData)
+
+    logI("get url success")
